ca106662cd4235f4661afc9e5869b9e77d9c9f8f
MdhaffarMariam <mariammdhaffar.94@gmail.com>
2022-07-14 12:32:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import FullName from "./Components/FullName/FullName"
+import Bio from "./Components/bio/Bio"
+import Profession from "./Components/profession/Profession"
 import './App.css';
+import propTypes from 'prop-types';
 
 function App() {
+  const tennis = {
+    FullName : 'Ons Jabeur' ,
+    bio: 'Ons Jabeur (arabe : أنس جابر), née le 28 août 1994 à Ksar Hellal, est une joueuse de tennis tunisienne, professionnelle depuis 2012, classée numéro 2 mondiale par la WTA en juin 2022.',
+    profession : 'Joueuse de tennis' 
+  }
+  const handleName=(name)=>alert(`my name is ${name}`)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <FullName ons = {tennis} jabeur ={handleName}/>
+    <Bio biblio={tennis}/>
+    <Profession prof = {tennis}/>
     </div>
   );
-}
 
+  }
+ App.defaultProps= {
+    Profession : 'tennis'
+  } ;
+  App.propTypes={
+ Bio: propTypes.String
+  };
 export default App;
